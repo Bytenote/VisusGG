40d7a5091a4eef76a9dec364b55477029bc417f2
Bytenote <patric.seidel2+github@gmail.com>
2023-02-02 18:08:39 +0100

    Removed 1.2.0 files
    - removed file remains of old structure

--- scripts/popup/components/colorPicker.js
+++
@@ -1,4 +0,0 @@
-export const setColorPickerValue = (parent, color) => {
-	parent.parentElement.style.background = color;
-	parent.value = color;
-};
--- scripts/popup/helpers/toggles.js
+++
@@ -1,9 +0,0 @@
-import { getSyncStorage } from '../../shared/storage';
-
-export const getToggleInfo = (label) => {
-	const toggles = getSyncStorage('toggles');
-
-	if (toggles?.length > 0) {
-		return toggles.find((toggle) => toggle.label === label);
-	}
-};
